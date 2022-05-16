a871a31192e3b7343f6dbdf62729bbbf248d2360
Carolina Nonato <csnonato@gmail.com>
2022-05-16 16:29:04 -0400

    questions loaded

---
+++ index.js
@@ -0,0 +1,7 @@
+const questions = [
+    "What have I learned?",
+    "What upset me?",
+    "What could have I done better?",
+    "What made me happy?",
+    "How many people have I helped today?"
+]
