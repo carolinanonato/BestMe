19f82816c0dbd11857ae7827b308a72af9130ab0
Carolina Nonato <csnonato@gmail.com>
2022-05-16 18:19:11 -0400

    recieving data

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const questions = [
     "What made me happy?",
     "How many people have I helped today?"
 ]
+
+
+const ask = (index = 0) => {
+    process.stdout.write(questions[index] + `\n\n\n\n`)
+}
+
+ask()
